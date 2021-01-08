56619b20dfeb89964f949c43c2320d95bcfcf7cc
falkTX <falktx@falktx.com>
2021-01-08 19:06:15 +0000

    Reply to a few test messages

--- system-control.c
+++ system-control.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <errno.h>
 #include <limits.h>
 #include <signal.h>
 #include <unistd.h>
@@ -17,8 +18,13 @@
 #include <systemd/sd-daemon.h>
 
 // TODO put this in spec file
+#define MOD_SYSTEM_SERIAL_CMD_PREFIX "sys_"
 #define MOD_SYSTEM_SERIAL_CMD_SIZE  7 /* "cmd_xyz" */
 #define MOD_SYSTEM_SERIAL_DATA_SIZE 2 /* "ff" max */
+
+#define MOD_SYSTEM_SERIAL_CMD_VERSION MOD_SYSTEM_SERIAL_CMD_PREFIX "ver"
+#define MOD_SYSTEM_SERIAL_CMD_BT_INFO MOD_SYSTEM_SERIAL_CMD_PREFIX "bti"
+#define MOD_SYSTEM_SERIAL_CMD_BT_DISC MOD_SYSTEM_SERIAL_CMD_PREFIX "btd"
 
 static volatile bool g_running = true;
 
@@ -119,14 +125,33 @@ static uint8_t serial_read_until_zero(struct sp_port* const serialport, char buf
 
     if (ret < MOD_SYSTEM_SERIAL_CMD_SIZE + 1)
     {
-        if (ret != SP_OK)
+        if (ret != SP_OK && (ret != 1 || buf[0] != '\0'))
             fprintf(stderr, "%s failed, reading command timed out or error, ret %d\n", __func__, ret);
+        return 0;
+    }
+
+    // check if message is valid
+    if (strncmp(buf, MOD_SYSTEM_SERIAL_CMD_PREFIX, strlen(MOD_SYSTEM_SERIAL_CMD_PREFIX)) != 0)
+    {
+#if 1
+        // TESTING we should not print invalid chars
+        buf[MOD_SYSTEM_SERIAL_CMD_SIZE] = '\0';
+        fprintf(stderr, "%s failed, invalid command '%s' receive\n", __func__, buf);
+#else
+        fprintf(stderr, "%s failed, invalid command received\n", __func__);
+#endif
         return 0;
     }
 
     // message was read in full (only has command), we can stop here
     if (buf[MOD_SYSTEM_SERIAL_CMD_SIZE] == '\0')
         return MOD_SYSTEM_SERIAL_CMD_SIZE;
+
+    if (buf[MOD_SYSTEM_SERIAL_CMD_SIZE] != ' ')
+    {
+        fprintf(stderr, "%s failed, command is missing space delimiter\n", __func__);
+        return 0;
+    }
 
     // message has more data on it, let's fetch the data size
     reading_offset += MOD_SYSTEM_SERIAL_CMD_SIZE + 1;
@@ -150,7 +175,7 @@ static uint8_t serial_read_until_zero(struct sp_port* const serialport, char buf
         // LONG_MAX;
         // LONG_MIN;
 
-        if (data_size <= 0 || data_size > 0xff - reading_offset)
+        if (data_size <= 0 || data_size > 0xff - reading_offset - 1)
         {
             fprintf(stderr, "%s failed, incorrect command data size '%s'\n", __func__, data_size_str);
             return 0;
@@ -180,6 +205,45 @@ static uint8_t serial_read_until_zero(struct sp_port* const serialport, char buf
     }
 
     return (uint8_t)total_msg_size;
+}
+
+static bool write_or_close(struct sp_port* serialport, const char* const resp)
+{
+    errno = 0;
+    if (sp_nonblocking_write(serialport, resp, strlen(resp)+1) == SP_ERR_FAIL && errno == EIO)
+        return false;
+    return true;
+}
+
+static bool parse_and_reply_to_message(struct sp_port* serialport, char msg[0xff])
+{
+    // WIP
+    if (strncmp(msg, MOD_SYSTEM_SERIAL_CMD_VERSION, MOD_SYSTEM_SERIAL_CMD_SIZE) == 0)
+    {
+        fprintf(stdout, "%s: replying version\n", __func__);
+
+        const char* const resp = "v1.10.1\n";
+        return write_or_close(serialport, resp);
+    }
+
+    if (strncmp(msg, MOD_SYSTEM_SERIAL_CMD_BT_INFO, MOD_SYSTEM_SERIAL_CMD_SIZE) == 0)
+    {
+        fprintf(stdout, "%s: replying bluetooth info\n", __func__);
+
+        const char* const resp = "this,that,what\n";
+        return write_or_close(serialport, resp);
+    }
+
+    if (strncmp(msg, MOD_SYSTEM_SERIAL_CMD_BT_DISC, MOD_SYSTEM_SERIAL_CMD_SIZE) == 0)
+    {
+        fprintf(stdout, "%s: replying bluetooth discovery\n", __func__);
+
+        const char* const resp = "ok\n";
+        return write_or_close(serialport, resp);
+    }
+
+    fprintf(stderr, "%s: unknown message '%s'\n", __func__, msg);
+    return true;
 }
 
 int main(int argc, char* argv[])
@@ -225,7 +289,8 @@ int main(int argc, char* argv[])
         if ((msg_size = serial_read_until_zero(serialport, buf)) == 0)
             continue;
 
-        fprintf(stdout, "DEBUG: read message ok:\n%s", buf);
+        if (! parse_and_reply_to_message(serialport, buf))
+            break;
     }
 
     // notify systemd we are stopping
